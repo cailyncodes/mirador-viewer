5ff7fefa374ee0e5fdbffe298816efdc9e28a4ba
Cole Hansen <cole_hansen@brown.edu>
2017-06-09 10:43:34 -0400

    Update build script The compiled version needs to point to the correct location for polymer, install gulp-htmlprocess to handle

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const gulp = require('gulp');
 const inlinesource = require('gulp-inline-source');
+const processhtml = require('gulp-processhtml');
 
 gulp.task('build', () => {
   return gulp.src('./src/mirador-viewer.html')
+    .pipe(processhtml({}))
     .pipe(inlinesource())
     .pipe(gulp.dest('./'));
 });
+
+gulp.task('default', ['build']);
